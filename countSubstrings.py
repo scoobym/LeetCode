c494d11c2a637020a230faa27eeedbe077c6f256
scoobym <mayuanbo77@gmail.com>
2018-03-20 00:03:23 +0800

    gi

---
+++ countSubstrings.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    count = 0
+
+    def isSubstrings(self, s, left, right):
+        while(left>=0 and right<len(s)) and (s[left]==s[right]):
+            left -= 1
+            right += 1
+            self.count += 1
+
+    def countSubstrings(self, s):
+        if len(s)==0:
+            return 0
+        for i in range(len(s)):
+            self.isSubstrings(s,i,i)
+            self.isSubstrings(s,i,i+1)
+
+        return self.count
+
+test = Solution()
+print(test.countSubstrings("aabcaa"))
